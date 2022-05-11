21aa38a09bde4f402fd2453847d1c28fe312925a
FerOsorio08 <A01026502@itesm.mx>
2022-05-11 12:19:33 -0500

    Add python example

---
+++ hello.py
@@ -0,0 +1,18 @@
+"""
+
+Simple Test in Python
+
+by Fernanda Osorio
+
+(estandares internacionales)
+
+"""
+
+
+def main():
+	print("Hello World!")
+	print("HAllO")
+	print("Ohayogozaimasu")
+	print("ayuda commits")
+	print("Hola")
+main()
